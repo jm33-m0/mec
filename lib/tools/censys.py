926b28feaf829f31ef67517d174ec1c083fe68e0
jm33-m0 <jm33.m0@gmail.com>
2018-01-05 12:13:33 +0800

    pep8 and minor improvements

--- lib/tools/censys.py
+++ lib/tools/censys.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
-import requests
 import json
-import sys
+
+import requests
+from lib.cli.console import debug_except
 
 
-
-
-
-def search_hosts(query,page):
+def search_hosts(query, page):
     key = json.loads(open("/usr/share/mec/conf/censys.conf", "r").read())
     API_URL = "https://censys.io/api/v1/search/ipv4"
     data = {
@@ -21,44 +19,46 @@ def search_hosts(query,page):
     try:
         results = requests.post(
             url=API_URL,
-            data=data_encoded, auth=(key['uid'],key['sec']))
+            data=data_encoded, auth=(key['uid'], key['sec']))
         results_list = json.loads(results.text)
         hosts = []
         for host in results_list['results']:
-            hosts.append((str(host['ip'])+":"+str(host['protocols'][0].split("/")[0])))
+            hosts.append((str(host['ip']) + ":" +
+                          str(host['protocols'][0].split("/")[0])))
         return hosts
-    except:
+    except BaseException:
         print("Oops something went wrong. check logs.")
+        debug_except()
 
 
+# def search_websites(query, page):
+#     # soon :)
+#     pass
 
-def search_websites(query,page):
-    # soon :)
-    return
 
+def run(query, pages):
 
-def run(query,pages):
-    
     i = 0
     hosts = []
     while i <= int(pages):
-        i+= 1
+        i += 1
 
         # multi thread causes temp ban.
-        hosts = hosts+search_hosts(query,i)
-    
-    out_name=query
-    
+        hosts = hosts + search_hosts(query, i)
+
+    out_name = query
+
     for special_ch in ['"', "'", ':', '!', '\\', '/']:
         if special_ch in out_name:
             out_name = out_name.replace(special_ch, ' ')
-    file = 'data/censys_'+out_name
+    file = 'data/censys_' + out_name
     out = open(file, "a")
-    print(str(len(hosts))+" Host found.")
+    print(str(len(hosts)) + " Host found.")
     out.write("\n".join(str(x) for x in hosts))
     return file
+
 
 def start():
     query = input("Search query: ")
     pages = input("Pages to crawl: ")
-    return run(query,pages)
+    return run(query, pages)
