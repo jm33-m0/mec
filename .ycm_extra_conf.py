0fe76173ae3b5728ab538cc7f2ab5cef87e6eef0
jm33-m0 <jm33.m0@gmail.com>
2020-02-28 12:50:55 +0800

    python virtualenv

--- .ycm_extra_conf.py
+++ .ycm_extra_conf.py
@@ -1,4 +1,7 @@
+import os
+
 def Settings(**kwargs):
+    venv_path = os.getcwd() + "/.venv"
     return {
-        'interpreter_path': './.venv/bin/python3'
+        'interpreter_path': venv_path + '/bin/python3'
     }
